98bd759105639e638d805a2f72e4b4465591a9cf
nrismawardah <nrismawardah@gmail.com>
2023-05-29 10:37:41 +0700

    Membuat display "orang dibuat dengan umur"

--- ProblemMultiWaris/ProblemMultiWaris.cpp
+++ ProblemMultiWaris/ProblemMultiWaris.cpp
@@ -1,7 +1,13 @@
-#include <isotream>
+#include <iostream>
 using namespace std;
 
 class orang {
 public:
 	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+	}
 };
