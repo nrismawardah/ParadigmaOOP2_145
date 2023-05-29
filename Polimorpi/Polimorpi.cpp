c638060d10615d36c53afc7bc90df7695329152b
nrismawardah <nrismawardah@gmail.com>
2023-05-29 11:24:58 +0700

    Mengembalikan nilai 0

--- Polimorpi/Polimorpi.cpp
+++ Polimorpi/Polimorpi.cpp
@@ -15,3 +15,25 @@ public:
 		cout << "Pesan dari Joko" << endl;
 	}
 };
+
+class lia : public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari lia" << endl;
+	}
+
+};
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	// a.seseorang::pesan();
+
+	return 0;
+}
