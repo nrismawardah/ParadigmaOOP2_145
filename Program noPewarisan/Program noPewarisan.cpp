06a471e5909c5ddce3d2b470b0acd7c2c764e872
nrismawardah <nrismawardah@gmail.com>
2023-05-29 13:14:29 +0700

    Terdapat error karena fungsi final

--- Program noPewarisan/Program noPewarisan.cpp
+++ Program noPewarisan/Program noPewarisan.cpp
@@ -7,3 +7,15 @@ public:
 		cout << "Hallo saya Function dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
